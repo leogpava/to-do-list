93a580949ea3b4ddc7b0706c671add1c4b2982a9
Leonardo Pavanelli <leogpava@gmail.com>
2025-09-04 15:56:59 -0300

    Beta

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 let lista_tarefas = []
 
+window.addEventListener("load", function(){
+    let store = localStorage.getItem("minhasTarefas")
+    if(store) { 
+        lista_tarefas = JSON.parse(store)
+        addHTML(lista_tarefas)
+    } else { 
+        addHTML(lista_tarefas)
+    }
+})  
+
+
 document.getElementById("add").addEventListener("click", function() {
     let input = document.getElementById("input-task")
     if (input.value === "") {window.alert("VAZIO")} else {
@@ -14,15 +25,20 @@ document.getElementById("add").addEventListener("click", function() {
     }
 })
 
-addHTML = (lista) => {
+const addHTML = (lista) => {
     document.getElementById("tarefas").innerHTML = ""
     lista.forEach((item, index) => {
+        let classe
+        let checked = item.status === "concluido" ? "checked" : ""
+
+        item.status === "concluido" ? classe = "done" : classe = "none"
+
         document.getElementById("tarefas").innerHTML += `
         <div class="task">
             <div id="esquerda">
-                <input type="checkbox" name="checkbox-${index}" id="checkbox-${index}">
+                <input type="checkbox" ${checked} name="checkbox-${index}" id="checkbox-${index}">
                 <label for="checkbox-${index}"></label>
-                <p>${item.task}</p>
+                <p class="${classe}">${item.task}</p>
             </div>
             <button id="apagar" data-index="${index}"><span class="material-symbols-outlined">close</span></button>
         </div>`
@@ -31,23 +47,32 @@ addHTML = (lista) => {
 
 document.getElementById("tarefas").addEventListener("click", function(event) {
     let botao = event.target.closest('button')
-    let indexBotao = botao.dataset.index
-    lista_tarefas.splice(indexBotao, 1)
-    addStorage(lista_tarefas)
-    addHTML(lista_tarefas)
-
-    //checkbox
     let checkbox = event.target.closest('input[type="checkbox"]')
-    if (checkbox == true) {
-        let index = checkbox.dataset.index
-        lista_tarefas[index] = {status: "concluido"}
+
+    if (botao) {
+        let indexBotao = botao.dataset.index
+        lista_tarefas.splice(indexBotao, 1)
         addStorage(lista_tarefas)
+        addHTML(lista_tarefas)
     }
+    
+    if (checkbox) {
+        let id = checkbox.id
+        let partes = id.split('-')
+        let index = partes[1]
+        let classe = ""
+        if (lista_tarefas[index].status === "pendente") {
+            lista_tarefas[index].status = "concluido"
+        } else { 
+            lista_tarefas[index].status = "pendente"
+        }
 
-
+        addStorage(lista_tarefas)
+        addHTML(lista_tarefas)
+    }
 })
 
-addStorage = (lista_tarefas) => {
+const addStorage = (lista_tarefas) => {
     let lista = JSON.stringify(lista_tarefas)
     localStorage.setItem("minhasTarefas", lista)
 }
